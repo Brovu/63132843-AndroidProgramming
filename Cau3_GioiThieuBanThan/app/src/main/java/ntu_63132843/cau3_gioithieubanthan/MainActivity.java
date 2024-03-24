217879bf8b14cb99761f45738922973ef108b3be
Brovu <roy772003@gmail.com>
2024-03-24 20:56:28 +0700

    them anh

--- Cau3_GioiThieuBanThan/app/src/main/java/ntu_63132843/cau3_gioithieubanthan/MainActivity.java
+++ Cau3_GioiThieuBanThan/app/src/main/java/ntu_63132843/cau3_gioithieubanthan/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu_63132843.cau3_gioithieubanthan;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ImageView img = findViewById(R.id.imageView);
+
+        img.setImageResource(R.mipmap.avt);
+
     }
 }
