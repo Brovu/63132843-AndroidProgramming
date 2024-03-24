b6a76e9d5e83b08d2e86a2d3aab50015c372bd38
Brovu <roy772003@gmail.com>
2024-03-24 20:29:40 +0700

    Lay thoi gian

--- Cau2_UngDungDongHo/app/src/main/java/ntu_63132843/cau2_ungdungdongho/MainActivity.java
+++ Cau2_UngDungDongHo/app/src/main/java/ntu_63132843/cau2_ungdungdongho/MainActivity.java
@@ -1,24 +1,34 @@
 package ntu_63132843.cau2_ungdungdongho;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.activity.EdgeToEdge;
+import androidx.fragment.app.FragmentTransaction;
+
 
 public class MainActivity extends AppCompatActivity {
+    private TextView hourDisplay;
+    private TextView minuteDisplay;
+    private TextView secondDisplay;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        hourDisplay = findViewById(R.id.hour_display);
+        minuteDisplay = findViewById(R.id.minute_display);
+        secondDisplay = findViewById(R.id.second_display);
+
     }
 }
