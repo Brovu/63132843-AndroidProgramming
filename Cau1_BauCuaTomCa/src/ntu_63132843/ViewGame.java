2aab114615f4df0bf4f76820b88f0e7e98a47daf
Brovu <roy772003@gmail.com>
2024-03-24 20:01:47 +0700

    tao ham roll dice

--- Cau1_BauCuaTomCa/src/ntu_63132843/ViewGame.java
+++ Cau1_BauCuaTomCa/src/ntu_63132843/ViewGame.java
@@ -128,4 +128,26 @@ public class ViewGame extends JFrame {
         btnBet.setBounds(155, 282, 134, 50);
         contentPane.add(btnBet);
     }
+    
+    private void rollDice() {
+        Map<String, Integer> diceResult = new HashMap<>();
+        diceResult.put("Tôm", 0);
+        diceResult.put("Cua", 0);
+        diceResult.put("Cá", 0);
+        diceResult.put("Bầu", 0);
+        diceResult.put("Gà", 0);
+        diceResult.put("Nai", 0);
+
+        int totalRolls = 0;
+        Random rand = new Random();
+        int totalWin = 0;
+        int totalLose = 0; // Biến để theo dõi tổng số tiền bị mất
+
+        int shrimpBet = Integer.parseInt(textFieldShrimp.getText());
+        int crabBet = Integer.parseInt(textFieldCrab.getText());
+        int fishBet = Integer.parseInt(textFieldFish.getText());
+        int gourdBet = Integer.parseInt(textFieldGourd.getText());
+        int roosterBet = Integer.parseInt(textFieldRooster.getText());
+        int deerBet = Integer.parseInt(textFieldDeer.getText());
+    }
 }
