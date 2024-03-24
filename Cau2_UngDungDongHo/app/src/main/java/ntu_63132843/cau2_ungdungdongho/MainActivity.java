c061ead56713f8412e55e1e1646d407a91c4cfa9
Brovu <roy772003@gmail.com>
2024-03-24 20:31:18 +0700

    them ham update thoi gian

--- Cau2_UngDungDongHo/app/src/main/java/ntu_63132843/cau2_ungdungdongho/MainActivity.java
+++ Cau2_UngDungDongHo/app/src/main/java/ntu_63132843/cau2_ungdungdongho/MainActivity.java
@@ -13,8 +13,8 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.activity.EdgeToEdge;
 import androidx.fragment.app.FragmentTransaction;
 
-
 public class MainActivity extends AppCompatActivity {
+
     private TextView hourDisplay;
     private TextView minuteDisplay;
     private TextView secondDisplay;
@@ -30,5 +30,29 @@ public class MainActivity extends AppCompatActivity {
         minuteDisplay = findViewById(R.id.minute_display);
         secondDisplay = findViewById(R.id.second_display);
 
+        updateTime();
+
+
+    }
+
+    private void updateTime() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SimpleDateFormat sdfHour = new SimpleDateFormat("HH", Locale.getDefault());
+                SimpleDateFormat sdfMinute = new SimpleDateFormat("mm", Locale.getDefault());
+                SimpleDateFormat sdfSecond = new SimpleDateFormat("ss", Locale.getDefault());
+
+                String currentHour = sdfHour.format(new Date());
+                String currentMinute = sdfMinute.format(new Date());
+                String currentSecond = sdfSecond.format(new Date());
+
+                hourDisplay.setText(currentHour);
+                minuteDisplay.setText(currentMinute);
+                secondDisplay.setText(currentSecond);
+
+                updateTime(); // Lặp lại sau mỗi giây
+            }
+        }, 1000); // Mỗi giây
     }
 }
